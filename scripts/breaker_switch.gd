02b4794e26ff91a49f5e43255a4c570fbb32f381
David McCool <mccoold7@gmail.com>
2024-10-11 16:03:58 -0400

    Expandable light breakers
    light switch that controls an array of lights that can be specified in the inspector menu

---
+++ scripts/breaker_switch.gd
@@ -0,0 +1,25 @@
+extends InteractableStaticBody3D
+
+
+var light_toggle: bool = true
+var y_movement: float = -1.3
+@export var controlled_lights: Array[toggle_light] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+	
+
+func interact(_body: CharacterBody3D) -> void:
+	light_toggle = !light_toggle
+	$breaker_switch.translate(Vector3(0, y_movement, 0))
+	y_movement *= -1
+	
+	for light in controlled_lights:
+		light.flip_state()
+	
---
+++ scripts/toggle_light.gd
@@ -0,0 +1,26 @@
+extends Node3D
+class_name toggle_light
+
+var light_strength: int = 1
+var toggle: bool = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+	
+func flip_state():
+	toggle = !toggle
+	if toggle:
+		$light_source.light_energy = light_strength
+		return
+	$light_source.light_energy = 0
+	
+	
+	
