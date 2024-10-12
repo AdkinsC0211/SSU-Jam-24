84a7d4955035a95db86758242a9b9b9727e3d3e5
David McCool <mccoold7@gmail.com>
2024-10-12 12:16:31 -0400

    Doors lockable and unlockable
    Doors and be locked and unlocked as well as opened and closed

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,6 +1,7 @@
 extends InteractableStaticBody3D
 
 var door_open: bool = false
+var door_locked: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,16 +10,29 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
-func interact(body: CharacterBody3D) -> void:
+func interact(_body: CharacterBody3D) -> void:
 	$AnimationPlayer.active = true
-	door_open = !door_open
+	
 	if door_open:
 		$AnimationPlayer.play_backwards("rotation",1)
-		interactMessage = "Press E to close door"
-	else:
+		
+	elif !door_locked:
 		$AnimationPlayer.play("rotation",-1,1,false)
+		
+	elif door_locked and door_open:
+		$AnimationPlayer.play_backwards("rotation",1)
+	else:
+		interactMessage = "Door is locked"
+		return
+		
+	door_open = !door_open
+	if door_open:
+		interactMessage = "Press E to close door"
+	elif !door_open and !door_locked:
 		interactMessage = "Press E to open door"
+	else:
+		interactMessage = "Door is locked"
 		
