a0bcf30aedaee4f6ca576a8daed3dc8f262b284d
David McCool <mccoold7@gmail.com>
2024-10-11 21:27:48 -0400

    Door functionality
    Doors snap open and closed

--- scripts/InteractableStaticBody3D.gd
+++ scripts/InteractableStaticBody3D.gd
@@ -2,8 +2,20 @@
 class_name InteractableStaticBody3D
 extends StaticBody3D
 
-@export var interactMessage := "Press E to Interact"
+@export var interactMessage := "Press E to open door"
+
+var door_open: bool = false
+
+
 
 # Virtual function that can be overridden by child classes
 func interact(body: CharacterBody3D) -> void:
-	print("Base interact: ", body)
+	var invert_rotation = 1
+	if door_open:
+		invert_rotation *= -1
+	self.global_rotate(Vector3(0,1,0),deg_to_rad(90 * invert_rotation))
+	door_open = !door_open
+	if !door_open:
+		interactMessage = "Press E to open door"
+	else:
+		interactMessage = "Press E to close door"
