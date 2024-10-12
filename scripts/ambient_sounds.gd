d7985d0313a58ef0bb5d1f42b58d33eab55740e5
AdkinsC0211 <adkinsc0211@gmail.com>
2024-10-12 14:31:27 -0400

    adding sounds to repo
    very nice

---
+++ scripts/ambient_sounds.gd
@@ -0,0 +1,75 @@
+extends Node3D
+
+var left_gain_mod = 0
+var right_gain_mod = 0
+
+@onready var audio_pan = AudioServer.get_bus_effect(0,0)
+
+
+var left_gain = 5
+var right_gain = 5
+
+var left_ear
+var right_ear
+
+var playing
+var to_play
+
+@onready var check_length = 1.41
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	left_ear = $LeftEar
+	right_ear = $RightEar
+	left_ear.play()
+	right_ear.play()
+	check_length = $LRayCast3D.target_position.length()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	left_ear.global_transform.origin = global_transform.origin + Vector3(-1,0,0)
+	right_ear.global_transform.origin = global_transform.origin + Vector3(1,0,0)
+	
+	left_gain_mod = 1
+	if $LRayCast3D.is_colliding():
+		left_gain_mod += ($LRayCast3D.get_collision_point() - $LRayCast3D.global_transform.origin).length() / 2
+	if $LRayCast3D2.is_colliding():
+		left_gain_mod += ($LRayCast3D.get_collision_point() - $LRayCast3D.global_transform.origin).length() / 2
+	
+	right_gain_mod = 1
+	if $RRayCast3D.is_colliding():
+		right_gain_mod += ($RRayCast3D.get_collision_point() - $RRayCast3D.global_transform.origin).length() / 2
+	if $RRayCast3D2.is_colliding():
+		right_gain_mod += ($RRayCast3D.get_collision_point() - $RRayCast3D.global_transform.origin).length() / 2
+	
+	audio_pan.pan = clampf(-1, 1, (check_length - right_gain_mod)/check_length - (check_length - left_gain_mod)/check_length)
+	
+func change_ambience(audiostream:AudioStream):
+	if audiostream != left_ear.stream:
+		var old_left = left_ear
+		var old_right = right_ear
+		left_ear = left_ear.duplicate()
+		right_ear = right_ear.duplicate()
+		
+		
+		left_ear.stream = audiostream
+		right_ear.stream = audiostream
+		
+		add_child(left_ear)
+		add_child(right_ear)
+		
+		left_ear.play()
+		right_ear.play()
+		
+		var temp = create_tween().set_parallel(true)
+		
+		temp.tween_property(left_ear, "volume_db", -20, 0)
+		temp.tween_property(right_ear, "volume_db", -20, 0)
+		temp.chain().tween_property(left_ear, "volume_db", 0, 3)
+		temp.set_parallel(true)
+		temp.tween_property(right_ear, "volume_db", 0, 3)
+		temp.tween_property(old_left, "volume_db", -20, 3)
+		temp.tween_property(old_right, "volume_db", -20, 3)
+		temp.chain().tween_callback(old_left.queue_free)
+		temp.tween_callback(old_right.queue_free)
