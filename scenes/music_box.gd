d9318fe0d32268a9b8a7ddfd5ddf1a071935ee98
Nightshade2023 <150634815+Nightshade2023@users.noreply.github.com>
2024-10-13 03:38:57 -0400

    salt, pepper, music box scene creation, use functions haven't been written

---
+++ scenes/music_box.gd
@@ -0,0 +1,8 @@
+extends StaticBody3D
+
+
+func use() -> void:
+	# do something
+	# rotation of $MusicBox/music_box_handle around z axis will do a wind animation
+	# rotation of $MusicBox/music_box_lid around z axis will open the box
+	pass
